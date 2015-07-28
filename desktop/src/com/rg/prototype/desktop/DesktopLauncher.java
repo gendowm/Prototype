11ea344f24d0caf9e895578750ec1f7e49d3047e
Ben <ocordu@gmail.com>
2015-07-28 21:23:56 +0100

    Initial commit

---
+++ desktop/src/com/rg/prototype/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rg.prototype.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rg.prototype.Application;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Application(), config);
+	}
+}
